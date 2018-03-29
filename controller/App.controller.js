933a98a232a728b94feeba5908815c2357fd5d45
Dmitry Fokin <fokind1@yandex.ru>
2018-03-29 17:00:01 +0300

    view Login отдельно

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,58 +1,10 @@
-/* global sap $ Cookies */
+/* global sap */
 'use strict';
 
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
-  'sap/ui/model/json/JSONModel',
-  'sap/m/MessageToast',
-], function(Controller, JSONModel, MessageToast) {
+], function(Controller) {
   return Controller.extend('tms.basic.controller.App', {
-    onInit: function() {
-      this.getView().setModel(new JSONModel());
-    },
-
-    onPressLogin: function() {
-      var oView = this.getView();
-      var oModel = oView.getModel();
-      var oResourceBundle = oView.getModel('i18n').getResourceBundle();
-
-      $.post(
-        {
-          url: $.sap.formatMessage('{0}Users/login', this.getOwnerComponent().getManifestEntry('/sap.app/dataSources/api/uri')),
-          data: $.sap.formatMessage('\'{\'"username":"{0}","password":"{1}"\'}\'', [oModel.getProperty('/username'), oModel.getProperty('/password')]),
-          contentType: 'application/json',
-        })
-      .done(function(data, status, xhr) {
-        Cookies.set('AccessToken', data);
-        MessageToast.show(oResourceBundle.getText('authSuccess'));
-      })
-      .fail(function(data) {
-        Cookies.remove('AccessToken');
-        MessageToast.show(oResourceBundle.getText('authError'));
-      });
-    },
-
-    onPressLogout: function() {
-      var oView = this.getView();
-      var oModel = oView.getModel();
-      var oResourceBundle = oView.getModel('i18n').getResourceBundle();
-      var oAccessToken = Cookies.getJSON('AccessToken');
-
-      if (oAccessToken) {
-        $.post(
-          {
-            url: $.sap.formatMessage('{0}Users/logout', this.getOwnerComponent().getManifestEntry('/sap.app/dataSources/api/uri')),
-            contentType: 'application/json',
-            headers: {'Authorization': oAccessToken.id},
-          })
-        .done(function(data, status, xhr) {
-          MessageToast.show(oResourceBundle.getText('authLogoutSuccess'));
-        })
-        .fail(function(data) {
-          MessageToast.show(oResourceBundle.getText('authLogoutSuccess'));
-        });
-        Cookies.remove('AccessToken');
-      }
-    },
+    onInit: function() {},
   });
 });
